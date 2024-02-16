22870255f3f229c1fe5db4da3fbb957e53e4b8e3
hutch28 <hutchinsjo@gcc.edu>
2024-02-16 12:24:09 -0500

    yet another line to main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi me");
         System.out.println("another line");
+        System.out.println("yet another line");
     }
 }
